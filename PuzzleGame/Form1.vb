68c725e3f003a9418a459d2cc950e93c6dc457ee
Raphael <psyruz18@gmail.com>
2022-12-03 20:16:08 +0800

    Add project files.

---
+++ PuzzleGame/Form1.vb
@@ -0,0 +1,87 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Module1.MovePiece(Button1, Button2)
+        Module1.MovePiece(Button1, Button4)
+        Module1.SolutionChecker()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Module1.MovePiece(Button2, Button1)
+        Module1.MovePiece(Button2, Button3)
+        Module1.MovePiece(Button2, Button5)
+        Module1.SolutionChecker()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Module1.MovePiece(Button3, Button2)
+        Module1.MovePiece(Button3, Button6)
+        Module1.SolutionChecker()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Module1.MovePiece(Button4, Button1)
+        Module1.MovePiece(Button4, Button5)
+        Module1.MovePiece(Button4, Button7)
+        Module1.SolutionChecker()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Module1.MovePiece(Button5, Button2)
+        Module1.MovePiece(Button5, Button4)
+        Module1.MovePiece(Button5, Button6)
+        Module1.MovePiece(Button5, Button8)
+        Module1.SolutionChecker()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Module1.MovePiece(Button6, Button3)
+        Module1.MovePiece(Button6, Button5)
+        Module1.MovePiece(Button6, Button9)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Module1.MovePiece(Button7, Button4)
+        Module1.MovePiece(Button7, Button8)
+        Module1.SolutionChecker()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Module1.MovePiece(Button8, Button5)
+        Module1.MovePiece(Button8, Button7)
+        Module1.MovePiece(Button8, Button9)
+        Module1.SolutionChecker()
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Module1.MovePiece(Button9, Button6)
+        Module1.MovePiece(Button9, Button8)
+        Module1.SolutionChecker()
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Dim tempArr() As String = {"1", "2", "3", "4", "5", "6", "7", "8", " "}
+        Randomize()
+        Module1.numberOfMoves = 0
+
+        For i As Integer = 0 To tempArr.Length - 1
+            Dim j As Integer
+            Dim temp As String
+            j = Math.Floor(Rnd() * (i + 1))
+            temp = tempArr(i)
+            tempArr(i) = tempArr(j)
+            tempArr(j) = temp
+        Next
+
+        For i = 0 To 8
+            Module1.puzzleButtons(i).Text = tempArr(i)
+            Module1.puzzleButtons(i).Enabled = True
+        Next
+
+        Button9.Enabled = True
+        Button11.Enabled = False
+
+
+
+    End Sub
+End Class
---
+++ PuzzleGame/Module1.vb
@@ -0,0 +1,50 @@
+﻿
+
+Module Module1
+    Public puzzleButtons() As Button = {Form1.Button1, Form1.Button2, Form1.Button3, Form1.Button4,
+                                        Form1.Button5, Form1.Button6, Form1.Button7, Form1.Button8, Form1.Button9}
+
+    Public numberOfMoves As Integer = 0
+
+    Sub MovePiece(ByRef btn1 As Button, ByRef btn2 As Button)
+        If btn2.Text = " " Then
+            btn2.Text = btn1.Text
+            btn1.Text = " "
+            numberOfMoves += 1
+        End If
+    End Sub
+
+    Sub SolutionChecker()
+        If Form1.Button1.Text = "1" And Form1.Button2.Text = "2" And
+                Form1.Button3.Text = "3" And Form1.Button4.Text = "4" And
+                Form1.Button5.Text = "5" And Form1.Button5.Text = "5" And
+                Form1.Button6.Text = "6" And Form1.Button7.Text = "7" And
+                Form1.Button8.Text = "8" Then
+            MsgBox("You solved it in " + CStr(numberOfMoves) + " moves")
+            For i = 0 To 8
+                puzzleButtons(i).Enabled = False
+            Next
+
+            Form1.Button9.Enabled = False
+            Form1.Button11.Enabled = True
+        End If
+
+        If Form1.Button1.Text = "1" And Form1.Button2.Text = "2" And
+        Form1.Button3.Text = "3" And Form1.Button4.Text = "4" And
+        Form1.Button5.Text = "5" And Form1.Button5.Text = "5" And
+        Form1.Button6.Text = "6" And Form1.Button7.Text = "8" And
+        Form1.Button8.Text = "7" Then
+            Form1.Button7.Text = "7"
+            Form1.Button8.Text = "8"
+
+            MsgBox("You solved it in " + CStr(numberOfMoves) + " moves")
+            For i = 0 To 8
+                puzzleButtons(i).Enabled = False
+            Next
+
+            Form1.Button9.Enabled = False
+            Form1.Button11.Enabled = True
+        End If
+    End Sub
+
+End Module
